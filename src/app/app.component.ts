7fdc4f0680727e9ed76067f60c5905825ff0240c
STM32F107VC <baer.sydney@outlook.com>
2024-10-15 21:32:59 +0200

    Added background color and DABubble logo to intro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from "./intro/intro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
